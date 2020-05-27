d915fdc7a941d984d4f3976b4fc497a2b2f060af
박지원 <jiwon0297@naver.com>
2020-05-27 11:09:54 +0900

    잠깐 중간작업
    ㅁㄴㅇㄹ

--- app/src/main/java/com/example/myapplication/mate/MateActivity.java
+++ app/src/main/java/com/example/myapplication/mate/MateActivity.java
@@ -1,16 +1,100 @@
 package com.example.myapplication.mate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.frag_home;
+import com.example.myapplication.ui.frag_mail;
+import com.example.myapplication.ui.frag_mypage;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MateActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private com.example.myapplication.ui.frag_home frag_home;
+    private com.example.myapplication.ui.frag_mail frag_mail;
+    private com.example.myapplication.ui.frag_mypage frag_mypage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mate);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.mail:
+                        setFrag(0);
+                        break;
+                    case R.id.home:
+                        setFrag(1);
+                        break;
+                    case R.id.mypage:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        frag_mail=new frag_mail();
+        frag_home=new frag_home();
+        frag_mypage=new frag_mypage();
+    }
+
+    public void alone(View v) {
+        Toast.makeText(this,"혼밥 메이트 구하기 화면으로 이동합니다.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, AloneActivity.class);
+        startActivity(intent);
+    }
+    public void study(View v) {
+        Toast.makeText(this,"스터디 메이트 구하기 화면으로 이동합니다.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, StudyActivity.class);
+        startActivity(intent);
+    }
+    public void contest(View v){
+        Toast.makeText(this,"공모전 메이트 구하기 화면으로 이동합니다.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ContestActivity.class);
+        startActivity(intent);
+    }
+    public void house(View v){
+        Toast.makeText(this,"하우스 메이트 구하기 화면으로 이동합니다.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, HouseActivity.class);
+        startActivity(intent);
+    }
+
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n) {
+            case 0:
+                ft.replace(R.id.Main_Frame, frag_mail);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.Main_Frame, frag_home);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.Main_Frame, frag_mypage);
+                ft.commit();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/mate/StudyActivity.java
+++ app/src/main/java/com/example/myapplication/mate/StudyActivity.java
@@ -2,15 +2,70 @@ package com.example.myapplication.mate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import com.example.myapplication.Main2Activity;
 import com.example.myapplication.R;
 
-public class StudyActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StudyActivity extends AppCompatActivity {
+    ArrayList<HashMap<String, String>> ContentList;
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
+
+        list=(ListView) findViewById(R.id.listView);
+        ContentList = new ArrayList<HashMap<String, String>>();
+
+        showList();
     }
+    public void view(View v){
+        Intent intent = new Intent(this, MateViewActivity.class);
+        startActivity(intent);
+    }
+
+    protected void showList(){
+        try{
+                String title = "같이 역전우동 드실 분 구해요";
+                String writer = "박지원";
+
+                HashMap<String, String> contents = new HashMap<String, String>();
+
+                contents.put("title", title);
+                contents.put("writer", writer);
+
+                boolean add = ContentList.add(contents);
+
+            ListAdapter adapter = new SimpleAdapter(
+                    StudyActivity.this, ContentList, R.layout.matelist_item,
+                    new String[]{"title","writer"},
+                    new int[]{R.id.title, R.id.writer}
+            );
+
+            list.setAdapter(adapter);
+        } catch (JSONException e){
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
 }
