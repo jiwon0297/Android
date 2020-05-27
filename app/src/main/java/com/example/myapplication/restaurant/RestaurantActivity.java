d9cc241fe5a1416b39aa46a9b76b2c4b4c65e07a
jinha <kjha7448@gmail.com>
2020-05-27 15:29:55 +0900

    112
    1313

--- app/src/main/java/com/example/myapplication/restaurant/RestaurantActivity.java
+++ app/src/main/java/com/example/myapplication/restaurant/RestaurantActivity.java
@@ -1,16 +1,104 @@
 package com.example.myapplication.restaurant;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.frag_home;
+import com.example.myapplication.ui.frag_mail;
+import com.example.myapplication.ui.frag_mypage;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class RestaurantActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private com.example.myapplication.ui.frag_home frag_home;
+    private com.example.myapplication.ui.frag_mail frag_mail;
+    private com.example.myapplication.ui.frag_mypage frag_mypage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.mail:
+                        setFrag(0);
+                        break;
+                    case R.id.home:
+                        setFrag(1);
+                        break;
+                    case R.id.mypage:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        frag_mail=new frag_mail();
+        frag_home=new frag_home();
+        frag_mypage=new frag_mypage();
+    }
+
+    public void cafe(View view) {
+        Toast.makeText(this,"카페 목록",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, CafeActivity.class);
+        startActivity(intent);
+    }
+
+    public void Korean(View view) {
+        Toast.makeText(this,"한식 목록",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, KoreanActivity.class);
+        startActivity(intent);
+    }
+
+    public void Chinese(View view) {
+        Toast.makeText(this,"중식 목록",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ChineseActivity.class);
+        startActivity(intent);
+    }
+
+    public void Japanese(View view) {
+        Toast.makeText(this,"일식 목록",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, JapaneseActivity.class);
+        startActivity(intent);
+    }
+
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n) {
+            case 0:
+                ft.replace(R.id.Main_Frame, frag_mail);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.Main_Frame, frag_home);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.Main_Frame, frag_mypage);
+                ft.commit();
+                break;
+        }
     }
 }
