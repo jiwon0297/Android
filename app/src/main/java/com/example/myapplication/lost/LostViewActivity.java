805764c281a1b042000f9147764727d93830507d
박지원 <jiwon0297@naver.com>
2020-05-27 17:23:11 +0900

    asdf
    asdf

--- app/src/main/java/com/example/myapplication/lost/LostViewActivity.java
+++ app/src/main/java/com/example/myapplication/lost/LostViewActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication.lost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.myapplication.R;
 
@@ -12,5 +14,13 @@ public class LostViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_view);
+
+        ImageButton Back = (ImageButton) findViewById(R.id.imageButton1);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
