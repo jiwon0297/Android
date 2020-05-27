671ed60d9b8c8c9f1089af1714f3e67f311afdfc
wjddnjs0407 <wjddnjs0407@naver.com>
2020-05-27 17:10:15 +0900

    우선올리깅

--- app/src/main/java/com/example/myapplication/lost/GetActivity.java
+++ app/src/main/java/com/example/myapplication/lost/GetActivity.java
@@ -1,15 +1,98 @@
 package com.example.myapplication.lost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
 import com.example.myapplication.R;
+import com.example.myapplication.ui.frag_home;
+import com.example.myapplication.ui.frag_mail;
+import com.example.myapplication.ui.frag_mypage;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class GetActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private com.example.myapplication.ui.frag_home frag_home;
+    private com.example.myapplication.ui.frag_mail frag_mail;
+    private com.example.myapplication.ui.frag_mypage frag_mypage;
+
+    private ListView listView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
+
+        String[] strDate = {"2020-04-07", "2020-05-27"};
+        int nCnt = 0;
+        ArrayList<ItemData> oData = new ArrayList<>();
+        for (int i=0;i<1000;i++){
+            ItemData oItem = new ItemData();
+            oItem.strTitle = "데이터 " + (i+1);
+            oItem.strDate = strDate[nCnt++];
+            oData.add(oItem);
+            if (nCnt >= strDate.length) nCnt = 0;
+
+        }
+        listView = (ListView)findViewById(R.id.listView);
+        ListAdapter oAdapter = new ListAdapter(oData);
+        listView.setAdapter(oAdapter);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.mail:
+                        setFrag(0);
+                        break;
+                    case R.id.home:
+                        setFrag(1);
+                        break;
+                    case R.id.mypage:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        frag_mail=new frag_mail();
+        frag_home=new frag_home();
+        frag_mypage=new frag_mypage();
+        setFrag(1); // 첫 프래그먼트 화면 지정
+    }
+
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n) {
+            case 0:
+                ft.replace(R.id.Main_Frame, frag_mail);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.Main_Frame, frag_home);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.Main_Frame, frag_mypage);
+                ft.commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/lost/ItemData.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.lost;
+
+public class ItemData {
+    public String strTitle;
+    public String strDate;
+}
