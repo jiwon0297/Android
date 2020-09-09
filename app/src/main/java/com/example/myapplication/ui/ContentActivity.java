bf4d9ec36792a5892b8999896d019aab3e343d62
박지원 <jiwon0297@naver.com>
2020-09-09 13:34:45 +0900

    f
    f

--- app/src/main/java/com/example/myapplication/ui/ContentActivity.java
+++ app/src/main/java/com/example/myapplication/ui/ContentActivity.java
@@ -1,24 +1,15 @@
 package com.example.myapplication.ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.mate.MateViewActivity;
-import com.example.myapplication.mate.MyAdapter;
-import com.example.myapplication.mate.SampleData;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/myapplication/ui/MypageActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MypageActivity.java
@@ -5,16 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
-import com.example.myapplication.mate.MateViewActivity;
-import com.example.myapplication.mate.MyAdapter;
-import com.example.myapplication.mate.SampleData;
-
-import java.util.ArrayList;
 
 public class MypageActivity extends AppCompatActivity {
     @Override
