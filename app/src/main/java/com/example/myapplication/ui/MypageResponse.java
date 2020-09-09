d12491333a2e192698fed4b476091e4f4d75ce03
tmdal5857 <63244032+tmdal5857@users.noreply.github.com>
2020-09-09 13:32:28 +0900

    sdf
    df

---
+++ app/src/main/java/com/example/myapplication/ui/MypageResponse.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.ui;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MypageResponse {
+    @SerializedName("code")
+    private int code;
+
+    @SerializedName("message")
+    private String message;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("nickname")
+    private String nickname;
+
+    @SerializedName("gender")
+    private String gender;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public String getGender() {
+        return gender;
+    }
+}
