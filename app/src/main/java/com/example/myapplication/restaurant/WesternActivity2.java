dc2a7cee2d3f3f86eef73d7283cb4537ec17f269
jinha <kjha7448@gmail.com>
2020-09-09 11:06:42 +0900

    dfs
    fds

---
+++ app/src/main/java/com/example/myapplication/restaurant/WesternActivity2.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WesternActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_western2);
+    }
+}
