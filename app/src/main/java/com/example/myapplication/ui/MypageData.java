1efdee9e8338f612c559fc8c6a2dba4deee2db61
tmdal5857 <63244032+tmdal5857@users.noreply.github.com>
2020-09-09 11:15:47 +0900

    asdf
    dff

--- app/src/main/java/com/example/myapplication/ui/MypageData.java
+++ app/src/main/java/com/example/myapplication/ui/MypageData.java
@@ -1,4 +1,14 @@
-package com.example.myapplication.ui;
+package com.example.myapplication.login;
+
+import com.google.gson.annotations.SerializedName;
 
 public class MypageData {
+    @SerializedName("email")
+    String email;
+
+
+    public MypageData(String email) {
+        this.email = email;
+    }
 }
+
