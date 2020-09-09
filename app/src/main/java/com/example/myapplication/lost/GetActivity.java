828e962a420e7927625305924f6e6c9451a3779a
wjddnjs0407 <wjddnjs0407@naver.com>
2020-09-09 18:24:09 +0900

    ttt

--- app/src/main/java/com/example/myapplication/lost/GetActivity.java
+++ app/src/main/java/com/example/myapplication/lost/GetActivity.java
@@ -5,43 +5,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.network.RetrofitClient;
+import com.example.myapplication.network.ServiceApi;
 import com.example.myapplication.ui.HomeActivity;
 import com.example.myapplication.ui.MailActivity;
 import com.example.myapplication.ui.MypageActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GetActivity extends AppCompatActivity {
-
-
+    private String type = "주웠어요";
+    private ServiceApi service;
+    private ProgressBar mProgressView;
     private ListView listView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
 
-        String[] strDate = {"2020-04-07", "2020-05-27"};
-        int nCnt = 0;
-        ArrayList<LostData> oData = new ArrayList<>();
-        for (int i=0;i<1000;i++){
-            LostData oItem = new LostData();
-            //oItem.strTitle = "데이터 " + (i+1);
-            //oItem.strDate = strDate[nCnt++];
-            oData.add(oItem);
-            if (nCnt >= strDate.length) nCnt = 0;
+        mProgressView = (ProgressBar) findViewById(R.id.loading);
 
-        }
-        listView = (ListView)findViewById(R.id.listView);
-        ListAdapter oAdapter = new ListAdapter(oData);
-        listView.setAdapter(oAdapter);
+        service = RetrofitClient.getClient().create(ServiceApi.class);
+
+        attemptList();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnNavigationItemSelectedListener(new GetActivity.ItemSelectedListener());
+
+    }
+
+    private void attemptList() {
+        startList(new LostData(type));
+    }
+
+    private void startList(LostData lostData) {
+        List<LostData> oData = new ArrayList<>();
+
+        service.lostList(lostData).enqueue(new Callback<LostResponse>(){
+            @Override
+            public void onResponse(Call<LostResponse> call, Response<LostResponse> response) {
+                LostResponse result = response.body();
+                Toast.makeText(GetActivity.this, result.getMessage(), Toast.LENGTH_SHORT).show();
+                showProgress(false);
+
+                if (result.getCode() == 200) {
+                    LostResponse sample = result;
+                    for (LostResponse a :sample.getResult() ){
+                        LostData oItem = new LostData();
+                        oItem.campus = "[" + a.getCampus() + "]";
+                        oItem.title = a.getTitle();
+                        oItem.id = a.getId();
+                        oData.add(oItem);
+                    }
+                    listView = (ListView)findViewById(R.id.listView);
+                    ListAdapter oAdapter = new ListAdapter((ArrayList<LostData>) oData);
+                    listView.setAdapter(oAdapter);
+
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LostResponse> call, Throwable t) {
+                Toast.makeText(GetActivity.this, "분실물 에러 발생", Toast.LENGTH_SHORT).show();
+                Log.e("분실물 에러 발생", t.getMessage());
+                t.printStackTrace();
+                showProgress(false);
+            }
+        });
 
     }
 
@@ -68,4 +114,7 @@ public class GetActivity extends AppCompatActivity {
         }
     }
 
+    private void showProgress(boolean show) {
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
 }
