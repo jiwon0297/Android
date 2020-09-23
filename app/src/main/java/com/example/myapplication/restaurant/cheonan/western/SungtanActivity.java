aed1de72d03b211eb271ba3a068f354a93de581a
jinha <kjha7448@gmail.com>
2020-09-23 20:33:28 +0900

    fsdfsdfsdf
    sdfsdfsdfsdfsdfs

--- app/src/main/java/com/example/myapplication/restaurant/cheonan/western/SungtanActivity.java
+++ app/src/main/java/com/example/myapplication/restaurant/cheonan/western/SungtanActivity.java
@@ -2,7 +2,15 @@ package com.example.myapplication.restaurant.cheonan.western;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,15 +21,103 @@ import com.example.myapplication.ui.MailActivity;
 import com.example.myapplication.ui.MypageActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class SungtanActivity extends AppCompatActivity {
+public class SungtanActivity extends AppCompatActivity implements ExpandableListView.OnGroupClickListener,
+        ExpandableListView.OnChildClickListener{
+    ExpandableListView listView;
+    String[] groups= new String[]{"성탄버거&도시락버거","요리&식사","식사&고기","사이드메뉴","음료"};
+    String[][] childs={{"스테이크 치아바타 버거\n(SIN) 6,900원\n(BIG) 8,900원","스테이크 라이스 버거\n(SIN) 6,900원\n(BIG) 8,900원","갈비 치아바타 버거\n(SIN) 5,900원\n(BIG) 7,900원","갈비 라이스버거\n(SIN) 5,900원\n(BIG) 7,900원","치아바타 샌드버거\n(SIN) 4,900원\n(BIG) 6,900원"},
+            {"수제등심 왕돈까스(1인분) 8,000원","안창스테이크철판구이\n(기본) 12,900원\n(한판) 27,900원","안창갈비철판구이\n(기본) 11,900원\n(한판) 25,900원","소고기듬뿍해장국(1인분) 6,500원","마늘돼지 석갈비(한판) 24,900원"},
+            {"메밀 물냉면 6,900원","메밀 비빔냉면 6,900원","갈비 철판물냉면 8,900원","갈비 비빔냉면 8,900원"},
+            {"모듬세트 24,000원\n(왕새우+치킨안심가라아게+치즈스틱+포테이토)","왕새우튀김(7마리) 9,000원","웨지감자튀김 8,000원","치킨안심가라아게(12조각) 8,000원","포테이토 5,000원","치즈스틱(5개) 5,000원"},
+            {"아메리카노 2,000원","카페라떼 2,500원","얼그레이티 3,000원","히비스커스티 3,500원","콜라(355ml) 1,500원","사이다(355ml) 1,500원","환타(355ml) 1,500원"}};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sungtan);
 
+        ExpandableListAdapter listAdapter = new SungtanActivity.MyExpandableListAdapter();
+        listView = (ExpandableListView)findViewById(R.id.expandableListView);
+        listView.setAdapter(listAdapter);
+        listView.setOnGroupClickListener(this);
+        listView.setOnChildClickListener(this);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnNavigationItemSelectedListener(new SungtanActivity.ItemSelectedListener());
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        return false;
+    }
+
+    @Override
+    public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+        return false;
+    }
+
+    public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+
+        public Object getChild(int groupPosition, int childPosition){
+            return childs[groupPosition][childPosition];
+        }
+
+        public long getChildId(int groupPosition, int childPosition){
+            return childPosition;
+        }
+
+        public int getChildrenCount(int groupPosition){
+            return childs[groupPosition].length;
+        }
+
+        public TextView getGenericView(){
+            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,64);
+            TextView textView = new TextView(SungtanActivity.this);
+            textView.setGravity(Gravity.CENTER_VERTICAL|Gravity.LEFT);
+            textView.setPadding(30,40,0,40);
+            textView.setTextSize(15);
+
+            return textView;
+        }
+
+        public View getChildView(int groupPosition, int childPosition,
+                                 boolean isLastChild, View convertView, ViewGroup parent){
+            TextView textView = getGenericView();
+            textView.setText(getChild(groupPosition,childPosition).toString());
+
+            return textView;
+        }
+
+        public Object getGroup(int groupPosition){
+            return groups[groupPosition];
+        }
+
+        public int getGroupCount(){
+            return groups.length;
+        }
+
+        public long getGroupId(int groupPosition){
+            return groupPosition;
+        }
+
+        public View getGroupView(int groupPosition,boolean isExpanded,
+                                 View convertView, ViewGroup parent){
+            TextView textView = getGenericView();
+            textView.setText(getGroup(groupPosition).toString());
+
+            listView.setGroupIndicator(null);
+
+            return textView;
+        }
+
+        public boolean isChildSelectable(int groupPosition, int childPosition){
+            return true;
+        }
+
+        public boolean hasStableIds(){
+            return true;
+        }
     }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
