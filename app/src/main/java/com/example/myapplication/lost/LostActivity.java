ab7b777bbfa824ca4ce2688413dbd4e21eb5009c
wjddnjs0407 <wjddnjs0407@naver.com>
2020-09-23 14:25:54 +0900

    losttt

--- app/src/main/java/com/example/myapplication/lost/LostActivity.java
+++ app/src/main/java/com/example/myapplication/lost/LostActivity.java
@@ -1,15 +1,22 @@
 package com.example.myapplication.lost;
 
+    import androidx.annotation.NonNull;
     import androidx.appcompat.app.AppCompatActivity;
 
     import android.content.Intent;
     import android.os.Bundle;
     import android.util.Log;
+    import android.view.MenuItem;
     import android.view.View;
     import android.widget.Button;
     import android.widget.Toast;
 
     import com.example.myapplication.R;
+    import com.example.myapplication.mate.MateActivity;
+    import com.example.myapplication.ui.HomeActivity;
+    import com.example.myapplication.ui.MailActivity;
+    import com.example.myapplication.ui.MypageActivity;
+    import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LostActivity extends AppCompatActivity {
     private final String NICKNAME_EXTRA = "NICKNAME_EXTRA";
@@ -40,7 +47,34 @@ public class LostActivity extends AppCompatActivity {
             }
         });
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new LostActivity.ItemSelectedListener());
     }
 
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.home:
+                    Intent intent = new Intent(LostActivity.this, HomeActivity.class);
+                    intent.putExtra(NICKNAME_EXTRA, getIntent().getStringExtra("NICKNAME_EXTRA"));
+                    startActivity(intent);
+                    break;
+                case R.id.mail:
+                    Intent intent2 = new Intent(LostActivity.this, MailActivity.class);
+                    intent2.putExtra(NICKNAME_EXTRA, getIntent().getStringExtra("NICKNAME_EXTRA"));
+                    startActivity(intent2);
+                    break;
+                case R.id.mypage:
+                    Intent intent3 = new Intent(LostActivity.this, MypageActivity.class);
+                    intent3.putExtra(NICKNAME_EXTRA, getIntent().getStringExtra("NICKNAME_EXTRA"));
+                    startActivity(intent3);
+                    break;
+            }
+            return true;
+        }
+    }
 }
 
