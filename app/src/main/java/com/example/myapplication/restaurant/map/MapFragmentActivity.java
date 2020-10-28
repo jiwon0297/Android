88ed5998025e8d5fc21a280dfc57e3ae7f7c939b
jinha <kjha7448@gmail.com>
2020-10-28 13:16:44 +0900

    dd
    dd

--- app/src/main/java/com/example/myapplication/restaurant/map/MapFragmentActivity.java
+++ app/src/main/java/com/example/myapplication/restaurant/map/MapFragmentActivity.java
@@ -1,13 +1,14 @@
 package com.example.myapplication.restaurant.map;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
@@ -15,15 +16,14 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
-//import com.naver.maps.map.widget.ScaleBarView;
-//import com.naver.maps.map.widget.ZoomControlView;
+
+import java.util.Vector;
 
 public class MapFragmentActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE=1000;
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
     private MapView mapView;
-    private static final String TAG = "MapFragmentActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,14 +80,10 @@ public class MapFragmentActivity extends AppCompatActivity implements OnMapReady
         mapView.onLowMemory();
     }
 
+    @UiThread
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         this.naverMap = naverMap;
-        Log.d(TAG, "onMapReady");
-
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(14142445.8557230, 4475024.9302127));
-        marker.setMap(naverMap);
 
         naverMap.setLocationSource(locationSource);
         naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
@@ -95,9 +91,68 @@ public class MapFragmentActivity extends AppCompatActivity implements OnMapReady
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setCompassEnabled(true);
         uiSettings.setScaleBarEnabled(true);
-        uiSettings.setZoomControlEnabled(true); 
+        uiSettings.setZoomControlEnabled(true);
         uiSettings.setLocationButtonEnabled(true);
+
+        markersPosition = new Vector<LatLng>();
+        markersPosition.add(new LatLng(37.5670135, 126.9783740));
+        markersPosition.add(new LatLng(14151438.8512501, 4484205.1367426));
+        markersPosition.add(new LatLng(14151563.9571675, 4484369.0583384));
+        markersPosition.add(new LatLng(14151049.7987194, 4484364.5796063));
+        markersPosition.add(new LatLng(14150892.1473485, 4484398.0208062));
+
+        // 카메라 이동 되면 호출 되는 이벤트
+        naverMap.addOnCameraChangeListener(new NaverMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(int reason, boolean animated) {
+                freeActiveMarkers();
+                // 정의된 마커위치들중 가시거리 내에있는것들만 마커 생성
+                LatLng currentPosition = getCurrentPosition(naverMap);
+                for (LatLng markerPosition: markersPosition) {
+                    if (!withinSightMarker(currentPosition, markerPosition))
+                        continue;
+                    Marker marker = new Marker();
+                    marker.setPosition(markerPosition);
+                    marker.setMap(naverMap);
+                    activeMarkers.add(marker);
+                }
+            }
+        });
     }
+
+    // 마커 정보 저장시킬 변수들 선언
+    private Vector<LatLng> markersPosition;
+    private Vector<Marker> activeMarkers;
+
+    // 현재 카메라가 보고있는 위치
+    public LatLng getCurrentPosition(NaverMap naverMap) {
+        CameraPosition cameraPosition = naverMap.getCameraPosition();
+        return new LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+    }
+
+    // 선택한 마커의 위치가 가시거리(카메라가 보고있는 위치 반경 3km 내)에 있는지 확인
+    public final static double REFERANCE_LAT = 1 / 109.958489129649955;
+    public final static double REFERANCE_LNG = 1 / 88.74;
+    public final static double REFERANCE_LAT_X3 = 3 / 109.958489129649955;
+    public final static double REFERANCE_LNG_X3 = 3 / 88.74;
+    public boolean withinSightMarker(LatLng currentPosition, LatLng markerPosition) {
+        boolean withinSightMarkerLat = Math.abs(currentPosition.latitude - markerPosition.latitude) <= REFERANCE_LAT_X3;
+        boolean withinSightMarkerLng = Math.abs(currentPosition.longitude - markerPosition.longitude) <= REFERANCE_LNG_X3;
+        return withinSightMarkerLat && withinSightMarkerLng;
+    }
+
+    // 지도상에 표시되고있는 마커들 지도에서 삭제
+    private void freeActiveMarkers() {
+        if (activeMarkers == null) {
+            activeMarkers = new Vector<Marker>();
+            return;
+        }
+        for (Marker activeMarker: activeMarkers) {
+            activeMarker.setMap(null);
+        }
+        activeMarkers = new Vector<Marker>();
+    }
+
 
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions, @NonNull int[] grantResults){
