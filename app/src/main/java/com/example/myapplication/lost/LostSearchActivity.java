77708b394b62ad1ee0a9c3a5875c8e767efd68f2
박지원 <jiwon0297@naver.com>
2020-10-28 14:48:16 +0900

    adfs
    afsd

---
+++ app/src/main/java/com/example/myapplication/lost/LostSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.lost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LostSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_search);
+    }
+}
