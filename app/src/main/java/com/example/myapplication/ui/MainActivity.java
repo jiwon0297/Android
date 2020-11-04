b385a84ab1f2adec486de8a37b6a9cb391bc361d
박지원 <jiwon0297@naver.com>
2020-11-04 14:12:12 +0900

    adf
    df

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,10 +13,19 @@ import com.example.myapplication.Join.JoinActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.login.LoginActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     Adapter adapter;
     ViewPager viewPager;
+
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 1500;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 5000; // time in milliseconds between successive task executions.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.view);
         adapter = new Adapter(this);
         viewPager.setAdapter(adapter);
+
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            @Override
+            public void run() {
+                if(currentPage == 6) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+
 
         Button loginbutton = (Button) findViewById(R.id.signin);
         loginbutton.setOnClickListener(new View.OnClickListener() {
