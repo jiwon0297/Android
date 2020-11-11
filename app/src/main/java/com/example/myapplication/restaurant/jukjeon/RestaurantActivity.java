4de4b11fa967392e845663b5d49048ec57bfd865
tmdal5857 <63244032+tmdal5857@users.noreply.github.com>
2020-11-11 15:32:11 +0900

    asdf
    asdf

--- app/src/main/java/com/example/myapplication/restaurant/jukjeon/RestaurantActivity.java
+++ app/src/main/java/com/example/myapplication/restaurant/jukjeon/RestaurantActivity.java
@@ -4,17 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
-import com.example.myapplication.restaurant.jukjeon.cafe.CafeActivity;
-import com.example.myapplication.restaurant.jukjeon.chinese.ChineseActivity;
 import com.example.myapplication.restaurant.jukjeon.japanese.JapaneseActivity;
-import com.example.myapplication.restaurant.jukjeon.korean.KoreanActivity;
-import com.example.myapplication.restaurant.jukjeon.western.WesternActivity;
 import com.example.myapplication.ui.HomeActivity;
 import com.example.myapplication.ui.MailActivity;
 import com.example.myapplication.ui.MypageActivity;
@@ -30,6 +26,15 @@ public class RestaurantActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnNavigationItemSelectedListener(new RestaurantActivity.ItemSelectedListener());
+
+
+        ViewGroup japanese = (ViewGroup) findViewById(R.id.japanese);
+        japanese.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent intent = new Intent(RestaurantActivity.this, JapaneseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
@@ -55,34 +60,6 @@ public class RestaurantActivity extends AppCompatActivity {
         }
     }
 
-    public void cafe(View view) {
-        Toast.makeText(this,"카페 목록",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, CafeActivity.class);
-        startActivity(intent);
-    }
 
-    public void Korean(View view) {
-        Toast.makeText(this,"한식 목록",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, KoreanActivity.class);
-        startActivity(intent);
-    }
-
-    public void Chinese(View view) {
-        Toast.makeText(this,"중식 목록",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, ChineseActivity.class);
-        startActivity(intent);
-    }
-
-    public void Japanese(View view) {
-        Toast.makeText(this,"일식 목록",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, JapaneseActivity.class);
-        startActivity(intent);
-    }
-
-    public void Western(View view) {
-        Toast.makeText(this,"양식 목록",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, WesternActivity.class);
-        startActivity(intent);
-    }
 
 }
