2359485d82eb2ae2c94edb5fea32df6215731cad
박지원 <jiwon0297@naver.com>
2020-11-25 22:22:37 +0900

    asdf
    asdf

--- app/src/main/java/com/example/myapplication/ui/SendMessageActivity.java
+++ app/src/main/java/com/example/myapplication/ui/SendMessageActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication.ui;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,10 +41,6 @@ public class SendMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(SendMessageActivity.this)
-                .setSmallIcon(R.drawable.bell)
-                .setContentTitle("알림 제목")
-                .setContentText("알림 내용!!");
         String recipient = getIntent().getStringExtra("RECIPIENT");
 
         RecipText = (TextView) findViewById(R.id.recipient);
@@ -64,25 +63,55 @@ public class SendMessageActivity extends AppCompatActivity {
         excuteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Bitmap mLargeIconForNoti = BitmapFactory.decodeResource(getResources(), R.drawable.bell);
-                PendingIntent mPendingIntent = PendingIntent.getActivity(SendMessageActivity.this, 0,
-                        new Intent(getApplicationContext(), SendMessageActivity.class),
-                        PendingIntent.FLAG_CANCEL_CURRENT
-                );
-                NotificationCompat.Builder mBuilder =
-                        new NotificationCompat.Builder(SendMessageActivity.this)
-                                .setSmallIcon(R.drawable.bell)
-                                .setContentTitle("쪽지 알림")
-                                .setContentText("[this진하]님께 쪽지가 도착했습니다.")
-                                .setDefaults(Notification.DEFAULT_SOUND)
-                                .setLargeIcon(mLargeIconForNoti)
-                                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                                .setAutoCancel(true)
-                                .setContentIntent(mPendingIntent);
-
                 NotificationManager mNotificationManager =
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                mNotificationManager.notify(0, mBuilder.build());
+                //알림(Notification)을 관리하는 관리자 객체를 운영체제(Context)로부터 소환하기
+                NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                //Notification 객체를 생성해주는 건축가객체 생성(AlertDialog 와 비슷)
+                NotificationCompat.Builder builder= null;
+
+                //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+                    String channelID="channel_01"; //알림채널 식별자
+                    String channelName="MyChannel01"; //알림채널의 이름(별명)
+
+                    //알림채널 객체 만들기
+                    NotificationChannel channel= new NotificationChannel(channelID,channelName,NotificationManager.IMPORTANCE_DEFAULT);
+
+                    //알림매니저에게 채널 객체의 생성을 요청
+                    notificationManager.createNotificationChannel(channel);
+
+                    //알림건축가 객체 생성
+                    builder=new NotificationCompat.Builder(SendMessageActivity.this, channelID);
+
+
+                }else{
+                    //알림 건축가 객체 생성
+                    builder= new NotificationCompat.Builder(SendMessageActivity.this, null);
+                }
+
+                //건축가에게 원하는 알림의 설정작업
+                builder.setSmallIcon(android.R.drawable.ic_menu_view);
+
+                //상태바를 드래그하여 아래로 내리면 보이는
+                //알림창(확장 상태바)의 설정
+                builder.setContentTitle("쪽지 알림");//알림창 제목
+                builder.setContentText("[this진하]님께 쪽지가 도착하였습니다.");//알림창 내용
+                //알림창의 큰 이미지
+                Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.bell);
+                builder.setLargeIcon(bm);//매개변수가 Bitmap을 줘야한다.
+
+                //건축가에게 알림 객체 생성하도록
+                Notification notification=builder.build();
+
+                //알림매니저에게 알림(Notify) 요청
+                notificationManager.notify(1, notification);
+
+                //알림 요청시에 사용한 번호를 알림제거 할 수 있음.
+                //notificationManager.cancel(1);
+
                 attemptSend();
             }
         });
